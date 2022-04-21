0902cd224c83992b506e971bce003be9f9254528
Вадим Фатеев <fateev1vv2@gmail.com>
2022-04-21 18:30:27 +0300

    task03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  startBtn: document.querySelector('button[type="submit"]')
 }
+let pos = 0;
+
+
+refs.form.addEventListener('submit', (e) => {
+  const stepTextContent = refs.step.value;
+  const amountTextContent = refs.amount.value;
+  e.preventDefault();
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+    const interval = setInterval((stepTextContent, amountTextContent) => {
+      pos += 1;
+      if (pos > refs.amount.value) {
+        clearInterval(interval)
+        pos = 0;
+        return
+      }
+
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+         Notiflix.Notify.success(`Fulfilled promise ${amountTextContent} in ${stepTextContent}ms`)
+
+      } else {
+        Notiflix.Notify.failure(`Rejected promise ${amountTextContent} in ${stepTextContent}ms`)
+      }
+    }, stepTextContent)}, refs.delay.value)
+  }
+  )
+})
