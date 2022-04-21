0a9eb142a5071d7579e49f65c1ef81167f293c39
Вадим Фатеев <fateev1vv2@gmail.com>
2022-04-21 11:42:02 +0300

    task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]')
+}
 
+let interval = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick)
+refs.stopBtn.addEventListener('click', onStopBtnClick)
+
+function onStartBtnClick() {
+    interval = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    
+    refs.startBtn.setAttribute('disabled', 'disabled');
+    refs.stopBtn.removeAttribute('disabled');
+}
+
+function onStopBtnClick() {
+    clearInterval(interval);
+       refs.stopBtn.setAttribute('disabled', 'disabled');
+    refs.startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
